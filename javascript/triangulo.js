e16e4cebe4d41c5f472950c2b1a03b24d62a03c3
Harold Burbano <haroldburbano2002@gmail.com>
2021-08-02 22:36:25 -0400

    Rotation for triangle finished

--- javascript/triangulo.js
+++ javascript/triangulo.js
@@ -22,11 +22,11 @@ rotation.addEventListener("click",()=>
         cleanCanvas()
         if(rAngle <= 2*Math.PI)
         {
-            rAngle += Math.PI/2
+            rAngle += Math.PI/10
             drawTriangle(l1,l2,l3,rAngle)
         }else
         {
-            rAngle = Math.PI/2
+            rAngle = Math.PI/10
             drawTriangle(l1,l2,l3,rAngle)
         }
     }
@@ -83,7 +83,7 @@ function drawTriangle(l1,l2,l3,rotationAngle)
 {
     const centerX = canvas.width/2
     const centerY = canvas.height/2
-    const side = 0.80*canvas.width
+    const side = 0.70*canvas.width
 
     let angle_1 = Math.acos((l3**2 + l2**2 - l1**2)/(2*l2*l3))
     let angle_2 = Math.acos((l3**2 + l1**2 - l2**2)/(2*l1*l3))
@@ -120,19 +120,24 @@ function drawTriangle(l1,l2,l3,rotationAngle)
     const Py3 = centerY - h/2
 
     //rotate the cordenates
-    const rPx1 = (Px1 - centerX)*Math.cos(rotationAngle) + (Py1 - centerY)*Math.sin(rotationAngle) + centerX
-    const rPy1 = (Px1 - centerX)*Math.sin(rotationAngle) + (Py1 - centerY)*Math.cos(rotationAngle) + centerY
-    const rPx2 = (Px2 - centerX)*Math.cos(rotationAngle) + (Py2 - centerY)*Math.sin(rotationAngle) + centerX
-    const rPy2 = (Px2 - centerX)*Math.sin(rotationAngle) + (Py2 - centerY)*Math.cos(rotationAngle) + centerY
-    const rPx3 = (Px3 - centerX)*Math.cos(rotationAngle) + (Py3 - centerY)*Math.sin(rotationAngle) + centerX
-    const rPy3 = (Px3 - centerX)*Math.sin(rotationAngle) + (Py3 - centerY)*Math.cos(rotationAngle) + centerY
+    const centerRotation = obtenerPuntoDeRotacion(Px1, Py1 , Px3 , Py3 , Px2 ,Py2 )
+    const rPx1 = (Px1 - centerRotation.x)*Math.cos(rotationAngle) - (Py1 - centerRotation.y)*Math.sin(rotationAngle) + centerRotation.x
+    const rPy1 = (Px1 - centerRotation.x)*Math.sin(rotationAngle) + (Py1 - centerRotation.y)*Math.cos(rotationAngle) + centerRotation.y
+    const rPx2 = (Px2 - centerRotation.x)*Math.cos(rotationAngle) - (Py2 - centerRotation.y)*Math.sin(rotationAngle) + centerRotation.x
+    const rPy2 = (Px2 - centerRotation.x)*Math.sin(rotationAngle) + (Py2 - centerRotation.y)*Math.cos(rotationAngle) + centerRotation.y
+    const rPx3 = (Px3 - centerRotation.x)*Math.cos(rotationAngle) - (Py3 - centerRotation.y)*Math.sin(rotationAngle) + centerRotation.x
+    const rPy3 = (Px3 - centerRotation.x)*Math.sin(rotationAngle) + (Py3 - centerRotation.y)*Math.cos(rotationAngle) + centerRotation.y
     console.log("rPx1 = " + rPx1 + " rPy1 = " + rPy1)
     ctx.beginPath()
     ctx.moveTo(rPx1,rPy1)
     ctx.lineTo(rPx2,rPy2)
     ctx.lineTo(rPx3,rPy3)
-    ctx.fillStyle = "#2ea811"
-    ctx.fill()
+    ctx.lineTo(rPx1,rPy1)
+    ctx.lineWidth = 3
+    ctx.strokeStyle =  "#2ea811"
+    ctx.stroke()
+    // ctx.fillStyle = "#2ea811"
+    // ctx.fill()
     ctx.closePath()
 }
 
@@ -160,4 +165,22 @@ function verificarTriangulo(l1,l2,l3)
         return false
     }
     return true
+}
+
+function obtenerPuntoDeRotacion(x1,y1,x2,y2,x3,y3)
+{
+    const m1 = (y1 - y2)/(x1 - x2)
+    const m2 = (y2 - y3)/(x2 - x3)
+    const b1 = y1 - m1*x1
+    const b2 = y2 - m2*x2
+    const puntoMedio1 = [(x1 + x2)/2,(y1 + y2)/2]
+    const puntoMedio2 = [(x3 + x2)/2,(y3 + y2)/2]
+    const m_mediana1 = (puntoMedio1[1] - y3)/(puntoMedio1[0] - x3)
+    const m_mediana2 = (puntoMedio2[1] - y1)/(puntoMedio2[0] - x1)
+    const b_mediana1 = y3 - m_mediana1*x3
+    const b_mediana2 = y1 - m_mediana2*x1
+    const px = (b_mediana2 - b_mediana1)/(m_mediana1 - m_mediana2)
+    const py = m_mediana1*px + b_mediana1
+    
+    return { x : px , y : py}
 }
